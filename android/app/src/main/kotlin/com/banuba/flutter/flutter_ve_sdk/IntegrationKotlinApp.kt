991a6699756b3f059bc6bf1717717338779fddb8
Ilya Pechuro <ilyapechuro991@gmail.com>
2021-06-07 17:41:28 +0300

    Migrate to 1.0.16

--- android/app/src/main/kotlin/com/banuba/flutter/flutter_ve_sdk/IntegrationKotlinApp.kt
+++ android/app/src/main/kotlin/com/banuba/flutter/flutter_ve_sdk/IntegrationKotlinApp.kt
@@ -3,7 +3,8 @@ package com.banuba.flutter.flutter_ve_sdk
 import com.banuba.flutter.flutter_ve_sdk.videoeditor.di.VideoEditorKoinModule
 import com.banuba.sdk.arcloud.di.ArCloudKoinModule
 import com.banuba.sdk.effectplayer.adapter.BanubaEffectPlayerKoinModule
-import com.banuba.sdk.token.storage.TokenStorageKoinModule
+import com.banuba.sdk.token.storage.di.TokenStorageKoinModule
+import com.banuba.sdk.gallery.di.GalleryKoinModule
 import io.flutter.app.FlutterApplication
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -17,10 +18,11 @@ class IntegrationKotlinApp : FlutterApplication() {
 
             // pass the customized Koin module that implements required dependencies.
             modules(
-                    VideoEditorKoinModule().module,
                     ArCloudKoinModule().module,
+                    TokenStorageKoinModule().module,
+                    VideoEditorKoinModule().module,
                     BanubaEffectPlayerKoinModule().module,
-                    TokenStorageKoinModule().module
+                    GalleryKoinModule().module
             )
         }
     }
