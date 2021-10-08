8d5f4ce166852c2962d36ae3a45301e3e7102b57
Alexander <alexander.bernat@banuba.com>
2021-10-08 15:41:25 +0300

    migrate to 1.0.18.1

--- android/app/src/main/kotlin/com/banuba/flutter/flutter_ve_sdk/videoeditor/di/VideoEditorKoinModule.kt
+++ android/app/src/main/kotlin/com/banuba/flutter/flutter_ve_sdk/videoeditor/di/VideoEditorKoinModule.kt
@@ -7,11 +7,11 @@ import com.banuba.flutter.flutter_ve_sdk.videoeditor.export.IntegrationAppExport
 import com.banuba.flutter.flutter_ve_sdk.videoeditor.impl.IntegrationAppWatermarkProvider
 import com.banuba.flutter.flutter_ve_sdk.videoeditor.impl.IntegrationTimerStateProvider
 import com.banuba.sdk.cameraui.data.CameraTimerStateProvider
+import com.banuba.sdk.export.data.ExportFlowManager
+import com.banuba.sdk.export.data.ExportParamsProvider
+import com.banuba.sdk.export.data.ForegroundExportFlowManager
 import com.banuba.sdk.ve.effects.WatermarkProvider
-import com.banuba.sdk.ve.flow.ExportFlowManager
 import com.banuba.sdk.ve.flow.FlowEditorModule
-import com.banuba.sdk.ve.flow.export.ForegroundExportFlowManager
-import com.banuba.sdk.veui.data.ExportParamsProvider
 import org.koin.core.definition.BeanDefinition
 import org.koin.core.qualifier.named
 
@@ -23,23 +23,23 @@ import org.koin.core.qualifier.named
  */
 class VideoEditorKoinModule : FlowEditorModule() {
 
-    override val exportFlowManager: BeanDefinition<ExportFlowManager> = single(override = true) {
+    val exportFlowManager: BeanDefinition<ExportFlowManager> = single(override = true) {
         ForegroundExportFlowManager(
             exportDataProvider = get(),
-            editorSessionHelper = get(),
+            sessionParamsProvider = get(),
+            exportSessionHelper = get(),
             exportDir = get(named("exportDir")),
-            mediaFileNameHelper = get(),
             shouldClearSessionOnFinish = true,
             publishManager = get(),
-            draftManager = get(),
-            errorParser = get()
+            errorParser = get(),
+            mediaFileNameHelper = get()
         )
     }
 
     /**
      * Provides params for export
      * */
-    override val exportParamsProvider: BeanDefinition<ExportParamsProvider> =
+    val exportParamsProvider: BeanDefinition<ExportParamsProvider> =
         factory(override = true) {
             IntegrationAppExportParamsProvider(
                 exportDir = get(named("exportDir")),
@@ -51,7 +51,7 @@ class VideoEditorKoinModule : FlowEditorModule() {
     /**
      * Provides path for exported files
      * */
-    override val exportDir: BeanDefinition<Uri> = single(named("exportDir"), override = true) {
+    val exportDir: BeanDefinition<Uri> = single(named("exportDir"), override = true) {
         get<Context>().getExternalFilesDir("")
             ?.toUri()
             ?.buildUpon()
@@ -59,7 +59,7 @@ class VideoEditorKoinModule : FlowEditorModule() {
             ?.build() ?: throw NullPointerException("exportDir should't be null!")
     }
 
-    override val watermarkProvider: BeanDefinition<WatermarkProvider> = factory(override = true) {
+    val watermarkProvider: BeanDefinition<WatermarkProvider> = factory(override = true) {
         IntegrationAppWatermarkProvider()
     }
 
