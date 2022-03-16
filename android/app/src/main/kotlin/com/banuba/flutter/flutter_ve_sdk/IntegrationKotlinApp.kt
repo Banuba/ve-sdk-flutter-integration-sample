3c9e18da3d08ebabb8bfcb10feaed58c683995c9
ve-sdk-android <73172228+ve-sdk-android@users.noreply.github.com>
2022-03-16 21:09:22 +0300

    migrate android to 1.22 (#20)
    Co-authored-by: Alexander <alexander.bernat@banuba.com>

--- android/app/src/main/kotlin/com/banuba/flutter/flutter_ve_sdk/IntegrationKotlinApp.kt
+++ android/app/src/main/kotlin/com/banuba/flutter/flutter_ve_sdk/IntegrationKotlinApp.kt
@@ -1,6 +1,6 @@
 package com.banuba.flutter.flutter_ve_sdk
 
-import com.banuba.flutter.flutter_ve_sdk.videoeditor.di.VideoEditorKoinModule
+import com.banuba.flutter.flutter_ve_sdk.videoeditor.di.IntegrationKoinModule
 import com.banuba.sdk.arcloud.di.ArCloudKoinModule
 import com.banuba.sdk.effectplayer.adapter.BanubaEffectPlayerKoinModule
 import com.banuba.sdk.export.di.VeExportKoinModule
@@ -8,6 +8,7 @@ import com.banuba.sdk.gallery.di.GalleryKoinModule
 import com.banuba.sdk.playback.di.VePlaybackSdkKoinModule
 import com.banuba.sdk.token.storage.di.TokenStorageKoinModule
 import com.banuba.sdk.ve.di.VeSdkKoinModule
+import com.banuba.sdk.ve.flow.di.VeFlowKoinModule
 import io.flutter.app.FlutterApplication
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -25,7 +26,8 @@ class IntegrationKotlinApp : FlutterApplication() {
                 VeExportKoinModule().module,
                 ArCloudKoinModule().module,
                 TokenStorageKoinModule().module,
-                VideoEditorKoinModule().module,
+                VeFlowKoinModule().module,
+                IntegrationKoinModule().module,
                 BanubaEffectPlayerKoinModule().module,
                 GalleryKoinModule().module,
                 VePlaybackSdkKoinModule().module
