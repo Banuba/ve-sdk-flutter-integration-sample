b347e8a39925c027da1509296ad6767646981e89
Egor Korshun <korshun.dev@gmail.com>
2022-08-23 13:26:16 +0300

    formatting_code / organize imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-// ignore: directives_ordering
-import 'dart:io' show Platform;
 
 void main() {
   runApp(MyApp());
