c1871e7b4f8202633719fa1882e4bd7b5dd26534
ve-sdk-android <sdk-ve-android@banuba.com>
2023-01-10 18:18:23 +0300

    Update migration to 1.26.3

--- android/app/src/main/kotlin/com/banuba/flutter/flutter_ve_sdk/SampleApp.kt
+++ android/app/src/main/kotlin/com/banuba/flutter/flutter_ve_sdk/SampleApp.kt
@@ -1,6 +1,7 @@
 package com.banuba.flutter.flutter_ve_sdk
 
 import io.flutter.app.FlutterApplication
+import android.util.Log
 import com.banuba.sdk.token.storage.license.BanubaVideoEditor
 
 class SampleApp : FlutterApplication() {
@@ -18,6 +19,18 @@ class SampleApp : FlutterApplication() {
         // Initialize Banuba VE UI SDK
         BanubaVideoEditorSDK().initialize(this@SampleApp)
 
-        BanubaVideoEditor.initialize(getString(R.string.banuba_token))
+        val videoEditorSDK = BanubaVideoEditor.initialize(getString(R.string.banuba_token))
+
+        if (videoEditorSDK == null) {
+            Log.e("BanubaVideoEditor", "BanubaVideoEditor initialization error")
+        } else {
+            videoEditorSDK.getLicenseState { isValid ->
+                if (isValid) {
+                    Log.d("BanubaVideoEditor", "BanubaVideoEditor token is valid")
+                } else {
+                    Log.d("BanubaVideoEditor", "BanubaVideoEditor token is not valid")
+                }
+            }
+        }
     }
 }
